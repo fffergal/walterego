7878902e57bb2264daeac8769376faf5d4c05f8f
Fergal <fergal+git@bfot.co.uk>
2021-04-27 19:06:12 +0100

    Add privacy focused analytics with Plausible.io

---
+++ pages/_document.jsx
@@ -0,0 +1,21 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async defer data-domain="walterego.golf" src="https://plausible.io/js/plausible.js" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
