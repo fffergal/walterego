2a2b8f52e4c6f714e293a4971529df99198da2f8
Fergal <fergal+git@bfot.co.uk>
2021-04-28 23:10:58 +0100

    Move sport mode features to component

---
+++ components/sport-mode-features.jsx
@@ -0,0 +1,71 @@
+import { CardColumns, Card } from 'react-bootstrap';
+
+export default function SportModeFeatures() {
+  return (
+    <CardColumns className="sport-columns">
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>Easy undo ↩️</Card.Title>
+          <Card.Text>
+            Save your existing profile info so it’s easy to switch back.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>Preview 🔍</Card.Title>
+          <Card.Text>
+            Preview your old white man before updating your profile.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>Fine detail 🎩</Card.Title>
+          <Card.Text>
+            Update your bio and background image too.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>Procedurally generated men 🎲</Card.Title>
+          <Card.Text>
+            Go beyond the stock men for a more unique profile.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>Favorite ❤️</Card.Title>
+          <Card.Text>
+            If you find a profile you really like, save it as your
+            favorite to use them again. They can be like your signature
+            old white man.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>Automatic timer ⏳</Card.Title>
+          <Card.Text>
+            Do you know what you’re about to say is going to attract the
+            wrong sort of attention? Set an automatic timer right before
+            you tweet, so you get all the benefit of using Walter Ego
+            without having to change your profile back yourself later.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="sml-card">
+        <Card.Body>
+          <Card.Title>White Knight 🐴</Card.Title>
+          <Card.Text>
+            Automatically switch profile when you receive a high number
+            of replies from accounts with low followers and egg profile
+            images.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardColumns>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import {
-  Row, Col, Card, CardColumns, Image, Button,
+  Row, Col, Image, Button,
 } from 'react-bootstrap';
 
 import MainTemplate from '../components/main-template';
+import SportModeFeatures from '../components/sport-mode-features';
 import TwitterCheck from '../components/twitter-check';
 
 export default function Home() {
@@ -43,71 +44,7 @@ export default function Home() {
         comes
         with these added extras:
       </p>
-      <CardColumns className="sport-columns">
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>Preview 🔍</Card.Title>
-            <Card.Text>
-              Preview your old white man before updating your profile.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>Easy undo ↩️</Card.Title>
-            <Card.Text>
-              Save your existing profile info so it’s easy to switch back.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>Fine detail 🎩</Card.Title>
-            <Card.Text>
-              Update your bio and background image too.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>Procedurally generated men 🎲</Card.Title>
-            <Card.Text>
-              Go beyond the stock men for a more unique profile.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>Favorite ❤️</Card.Title>
-            <Card.Text>
-              If you find a profile you really like, save it as your
-              favorite to use them again. They can be like your signature
-              old white man.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>Automatic timer ⏳</Card.Title>
-            <Card.Text>
-              Do you know what you’re about to say is going to attract the
-              wrong sort of attention? Set an automatic timer right before
-              you tweet, so you get all the benefit of using Walter Ego
-              without having to change your profile back yourself later.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="sml-card">
-          <Card.Body>
-            <Card.Title>White Knight 🐴</Card.Title>
-            <Card.Text>
-              Automatically switch profile when you receive a high number
-              of replies from accounts with low followers and egg profile
-              images.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardColumns>
+      <SportModeFeatures />
     </MainTemplate>
   );
 }
