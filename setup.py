1aa07662215e8baf3266bf44c9f13daccbe0416b
Fergal <fergal+git@bfot.co.uk>
2021-04-28 14:21:49 +0100

    Add tracing with OpenTelemetry and Honeycomb

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     install_requires=[
         "Authlib~=0.15",
         "Flask~=1.1",
+        "grpcio~=1.37",
+        "opentelemetry-exporter-otlp~=1.1",
+        "opentelemetry-instrumentation-flask~=0.20b0",
+        "opentelemetry-instrumentation-requests~=0.20b0",
+        "opentelemetry-sdk~=1.1",
         "requests~=2.25",
     ],
 )
--- walterego/app.py
+++ walterego/app.py
@@ -1,8 +1,16 @@
 """Create and configure the Flask app."""
 
 import os
+from types import SimpleNamespace
 
 from flask import Flask
+from grpc import ssl_channel_credentials
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from walterego.auth import oauth
 from walterego.blueprints import (
@@ -13,16 +21,60 @@ from walterego.blueprints import (
 )
 
 ENV_SECRETS = [
+    "HONEYCOMB_WRITE_KEY",
     "SECRET_KEY",
     "TWITTER_CLIENT_ID",
     "TWITTER_CLIENT_SECRET",
 ]
+
+# OpenTelemetry uses globals, so need to keep track of tracing init globally instead of
+# app context.
+tracing_inited_holder = SimpleNamespace(inited=False)
+
+
+def add_tracing(app):
+    """
+    Add tracing to a Flask app.
+
+    This is a bit different to the OpenTelemetry Python examples because Vercel does
+    some forking and it needs to be handled without knowing which app runner is being
+    used.
+    """
+    RequestsInstrumentor().instrument()
+    FlaskInstrumentor().instrument_app(app)
+    old_wsgi_app = app.wsgi_app
+
+    def init_tracing_app(environ, start_response):
+        if not tracing_inited_holder.inited:
+            otlp_exporter = OTLPSpanExporter(
+                endpoint="https://api.honeycomb.io:443",
+                insecure=False,
+                credentials=ssl_channel_credentials(),
+                headers=(
+                    ("x-honeycomb-team", app.config["HONEYCOMB_WRITE_KEY"]),
+                    ("x-honeycomb-dataset", "Walter Ego"),
+                ),
+            )
+            trace.set_tracer_provider(TracerProvider())
+            tracing_inited_holder.processor = BatchSpanProcessor(otlp_exporter)
+            trace.get_tracer_provider().add_span_processor(
+                tracing_inited_holder.processor
+            )
+            tracing_inited_holder.inited = True
+        try:
+            return old_wsgi_app(environ, start_response)
+        finally:
+            tracing_inited_holder.processor.force_flush()
+
+    app.wsgi_app = init_tracing_app
 
 
 def create_app():
     """Create Flask app, configure from env vars, register blueprints."""
     app = Flask(__name__)
     app.config.update({key: os.environ[key] for key in ENV_SECRETS})
+
+    add_tracing(app)
     oauth.init_app(app)
 
     app.register_blueprint(stockprofile.bp)
