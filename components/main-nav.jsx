00baa4b2bc94e15d125b93d4f613df349d28f7c5
Fergal <fergal+git@bfot.co.uk>
2021-05-01 18:05:07 +0100

    Add link to home from brand

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,18 +1,23 @@
+import Link from 'next/link';
 import { Nav, Navbar } from 'react-bootstrap';
 
 export default function MainNav() {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand>
-        <img
-          alt=""
-          src="/walterego.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />
-        {' '}
-        Walter Ego
+        <Link href="/">
+          <a className="text-reset text-decoration-none">
+            <img
+              alt=""
+              src="/walterego.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {' '}
+            Walter Ego
+          </a>
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
