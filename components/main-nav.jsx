81cbfb470a201068ee7c0b509a902ae074158ddf
Fergal <fergal+git@bfot.co.uk>
2021-08-29 02:33:05 +0100

    Update to Bootstrap 5
    Need to add Container to Nav, and replace CardColumn with Masonry.

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,32 +1,34 @@
 import Link from 'next/link';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export default function MainNav() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand>
-        <Link href="/">
-          <a className="text-reset text-decoration-none">
-            <img
-              alt=""
-              src="/walterego.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            {' '}
-            Walter Ego
-          </a>
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-        <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Container fluid>
+        <Navbar.Brand>
+          <Link href="/">
+            <a className="text-reset text-decoration-none">
+              <img
+                alt=""
+                src="/walterego.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              {' '}
+              Walter Ego
+            </a>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
--- components/sport-mode-features.jsx
+++ components/sport-mode-features.jsx
@@ -1,80 +1,111 @@
-import { CardColumns, Card } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 
 export default function SportModeFeatures() {
+  if (typeof window !== 'undefined') {
+    useEffect(() => {
+      const msnryPromise = (async () => {
+        const Masonry = (await import('masonry-layout')).default;
+        return new Masonry('.sports-columns', { percentagePosition: true, transitionDuration: 0 });
+      })();
+      return () => {
+        (async () => {
+          const msnry = await msnryPromise;
+          msnry.destroy();
+        })();
+      };
+    }, []);
+  }
   return (
-    <CardColumns className="sport-columns">
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Easy undo ↩️</Card.Title>
-          <Card.Text>
-            Save your existing profile info so it’s easy to switch back.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Preview 🔍</Card.Title>
-          <Card.Text>
-            Preview your old white man before updating your profile.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Fine detail 🎩</Card.Title>
-          <Card.Text>
-            Update your bio and background image too.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Procedurally generated men 🎲</Card.Title>
-          <Card.Text>
-            Go beyond the stock men for a more unique profile.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Favorite ❤️</Card.Title>
-          <Card.Text>
-            If you find a profile you really like, save it as your
-            favorite to use them again. They can be like your signature
-            old white man.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Automatic timer ⏳</Card.Title>
-          <Card.Text>
-            Do you know what you’re about to say is going to attract the
-            wrong sort of attention? Set an automatic timer right before
-            you tweet, so you get all the benefit of using Walter Ego
-            without having to change your profile back yourself later.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>White Knight 🐴</Card.Title>
-          <Card.Text>
-            Automatically switch profile when you receive a high number
-            of replies from accounts with low followers and egg profile
-            images.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="sml-card">
-        <Card.Body>
-          <Card.Title>Father figure ⚾️</Card.Title>
-          <Card.Text>
-            A paternal bot gently but firmly sets your chosen reply-guy
-            straight. Some people won’t listen to anyone else.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </CardColumns>
+    <Row className="sports-columns" sm={2} lg={3}>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Easy undo ↩️</Card.Title>
+            <Card.Text>
+              Save your existing profile info so it’s easy to switch back.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Preview 🔍</Card.Title>
+            <Card.Text>
+              Preview your old white man before updating your profile.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Fine detail 🎩</Card.Title>
+            <Card.Text>
+              Update your bio and background image too.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Procedurally generated men 🎲</Card.Title>
+            <Card.Text>
+              Go beyond the stock men for a more unique profile.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Favorite ❤️</Card.Title>
+            <Card.Text>
+              If you find a profile you really like, save it as your
+              favorite to use them again. They can be like your signature
+              old white man.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Automatic timer ⏳</Card.Title>
+            <Card.Text>
+              Do you know what you’re about to say is going to attract the
+              wrong sort of attention? Set an automatic timer right before
+              you tweet, so you get all the benefit of using Walter Ego
+              without having to change your profile back yourself later.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>White Knight 🐴</Card.Title>
+            <Card.Text>
+              Automatically switch profile when you receive a high number
+              of replies from accounts with low followers and egg profile
+              images.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="sml-card">
+          <Card.Body>
+            <Card.Title>Father figure ⚾️</Card.Title>
+            <Card.Text>
+              A paternal bot gently but firmly sets your chosen reply-guy
+              straight. Some people won’t listen to anyone else.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 }
